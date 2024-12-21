36d38143a0c247fb7df6ea4e3bcbbb9d4044aab7
adonis.silva <adonisinoda07@gmail.com>
2024-12-21 14:18:53 -0300

    feat: adiciona componente de tabela

---
+++ src/app/components/Tabela.tsx
@@ -0,0 +1,40 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { Estabelecimento } from "../types/estabelecimento";
+
+interface TableProps {
+  columns: string[][];
+  data: { [key: string]: any }[];
+  onClickRow:(row:Estabelecimento)=> void
+}
+
+export default function Tabela({ columns, data }: TableProps) {
+  return (
+    <div className="overflow-hidden">
+      <table className="min-w-full table-auto">
+        <thead className="bg-gray-100">
+          <tr>
+            {columns[1].map((column, index) => (
+              <th key={index} className="px-4 py-2 text-left font-semibold text-gray-700">
+                {column}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((row, index) => (
+            <tr key={index} className={`border-t ${index % 2 === 0 ? "bg-white" : "bg-gray-200"} hover:scale-[101%] hover:cursor-pointer hover:bg-blue-200`}>
+              {columns[0].map((column, idx) => (
+                <td key={idx} className="px-4 py-2 text-gray-600">
+                  {row[column] || "-"}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+
