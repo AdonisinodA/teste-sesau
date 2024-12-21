f3704170b4156d88cf5eceb0b382bfb9b86bc89f
adonis.silva <adonisinoda07@gmail.com>
2024-12-21 14:17:51 -0300

    feat: adiciona service de chamada ao endpoint de estabelecimento

---
+++ src/app/service/estabelecimento/Estabelecimento.ts
@@ -0,0 +1,36 @@
+import { Estabelecimento } from "../../types/estabelecimento";
+import api from "../api";
+
+export interface responseListarEstabelecimento{
+  estabelecimentos:Estabelecimento[]
+}
+
+class EstabelecimentoApi{
+
+    async listarEstabelecimentos() {
+        try {
+          const response = await api.get<responseListarEstabelecimento>("cnes/estabelecimentos?limit=100&offset=1");
+          return response.data; 
+        } catch (error) {
+          console.error("Erro ao listar os estabelecimentos:", error);
+          throw error; 
+        }
+      }
+
+
+
+
+    async buscarEstabelecimento(codigo_cnes:number) {
+      try {
+        const response = await api.get(`/cnes/estabelecimentos/${codigo_cnes}`);
+        return response.data; 
+      } catch (error) {
+        console.error("Erro ao buscar estabelecimento:", error);
+        throw error; 
+      }
+    }
+}
+
+
+const estabelecimentoApi = new EstabelecimentoApi() 
+export default estabelecimentoApi
